3a8491bb5e14a38261e5829700a394697d126ebe
kei-harada-personal <cashfeg@gmail.com>
2019-05-25 14:12:24 +0900

    Update __init__.py

--- aiwolfpy/__init__.py
+++ aiwolfpy/__init__.py
@@ -1,8 +1,4 @@
 from __future__ import print_function, division 
-from .tcpipclient import connect
-from .tcpipclient_parsed import connect_parse
-from . import templatetalkfactory 
-from . import templatewhisperfactory 
 from . import contentbuilder 
 from .gameinfoparser import GameInfoParser
 from .agent_proxy import AgentProxy
