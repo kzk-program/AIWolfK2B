b17f79cae5c412832dc81fc24e77b74fd35610dd
Kazuki Takashima <carzookey923@gmail.com>
2023-04-29 16:27:32 +0900

    gpt3_agent.pyのディレクトリ位置を変えてもaiwolfpyが正しくimportする

--- GPT_end_to_end/gpt3_agent.py
+++ GPT_end_to_end/gpt3_agent.py
@@ -3,11 +3,16 @@
 
 #This code is originated from python_simple_sample.py
 
+
+from logging import getLogger, StreamHandler, Formatter, FileHandler
+import sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+sys.path.append(str(pathlib.Path(__file__).parent.parent) + "\\aiwolfpy")
+from pathlib import Path
+import aiwolfpy
 import logging
 import openai
 import Levenshtein
-from logging import getLogger, StreamHandler, Formatter, FileHandler
-import aiwolfpy
 import argparse
 from time import sleep
 # name
@@ -67,7 +72,7 @@ class GameInfoAccumulater:
 # GPT3
 class AIChat:
     def __init__(self):
-        with open("../openai_api_key.txt", "r") as f:
+        with open(Path(__file__).resolve().parent.parent / 'openAIAPIkey.txt', "r") as f:
             openai.api_key = f.read()
         #log_paths = ['./werewolf_jp_examples/examples_for_gptinput_1.txt']
         log_paths = []
@@ -80,20 +85,23 @@ class AIChat:
     
     def speak(self, context):
         #send context to GPT-3, and return response
+        print("sending to GPT-3")
         response = openai.Completion.create(engine="text-davinci-003",
             prompt=self.examples+context,
             max_tokens=50,
             temperature=0.5)
-        with open("log_sending_to_gpt.txt", 'a', encoding="utf-8") as f:
+        
+        with open("./log_sending_to_gpt.txt", 'a', encoding="utf-8") as f:
             f.write("---------------\n")
             f.write("sent context:\n")
             f.write(self.examples + context)
             f.write("\n")
             f.write("response:\n")
             f.write(response['choices'][0]['text'])
-        return response['choices'][0]['text']
+        #return response['choices'][0]['text']
         # sleep(1)
-        # return "a"
+        print("sent to GPT-3")
+        return "a"
 
 
 # 
