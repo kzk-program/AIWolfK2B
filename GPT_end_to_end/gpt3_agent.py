5f0a01633f0172d153370c4bc63ecabe4a581692
kzk-program <carzookey923@docomo.ne.jp>
2023-04-30 17:27:11 +0900

    GPTの出力を実際に発言する。動作確認完了。

--- GPT_end_to_end/gpt3_agent.py
+++ GPT_end_to_end/gpt3_agent.py
@@ -91,21 +91,24 @@ class AIChat:
             max_tokens=50,
             temperature=0.5)
         
-        with open("./log_sending_to_gpt.txt", 'a', encoding="utf-8") as f:
-            f.write("---------------\n")
-            f.write("sent context:\n")
-            f.write(self.examples + context)
-            f.write("\n")
-            f.write("response:\n")
-            f.write(response['choices'][0]['text'])
-        #return response['choices'][0]['text']
-        # sleep(1)
+        # GPTとの通信内容を保存しておく
+        # with open(Path(__file__).resolve().parent / 'log_sending_to_gpt/.txt', 'a', encoding="utf-8") as f:
+        #     f.write("---------------\n")
+        #     f.write("sent context:\n")
+        #     f.write(self.examples + context)
+        #     f.write("\n")
+        #     f.write("response:\n")
+        #     f.write(response['choices'][0]['text'])
+        #     f.write("\n----------------\n")
         print("sent to GPT-3")
-        return "a"
+        return response['choices'][0]['text']
+        # sleep(1)
+        
+        
 
 
 # 
-class SampleAgent(object):
+class GPT3Agent(object):
     
     def __init__(self):
         # my name
@@ -182,7 +185,7 @@ class SampleAgent(object):
         return None
     
 
-agent = SampleAgent()
+agent = GPT3Agent()
 
 # read args
 parser = argparse.ArgumentParser(add_help=False)
