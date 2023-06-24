e6c1f85f88290627eef42e49eb8e2e018f9a7a9f
kzk-program <carzookey923@docomo.ne.jp>
2023-06-24 13:44:39 +0900

    とりあえずSimpleRequestProcessingModuleからそのままコピペ

---
+++ aiwolfk2b/AttentionReasoningAgent/Modules/RequestProcessingModule.py
@@ -0,0 +1,15 @@
+from configparser import ConfigParser
+from typing import List,Tuple,Dict,Any,Union
+
+from aiwolf import GameInfo, GameSetting
+from aiwolf.agent import Agent,Role
+from aiwolfk2b.AttentionReasoningAgent.AbstractModules import AbstractRoleEstimationModel,AbstractRequestProcessingModule,AbstractStrategyModule,OneStepPlan,ActionType
+
+class RequestProcessingModule(AbstractRequestProcessingModule):
+    """要求が来た場合、テキトーなことをいうモジュール"""
+    def __init__(self,config:ConfigParser,role_estimation_model: AbstractRoleEstimationModel, strategy_module:AbstractStrategyModule) -> None:
+        super().__init__(config,role_estimation_model,strategy_module)
+        
+    def process_request(self, game_info: GameInfo, game_setting: GameSetting) -> OneStepPlan:
+        plan = OneStepPlan("なんとなく(from request processing module)",ActionType.TALK,"お前には従わない(from request processing module)")
+        return plan
