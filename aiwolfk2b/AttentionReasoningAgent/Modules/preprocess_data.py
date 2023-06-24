eefcb87b085b48f5676b810809e0b168456f441a
HoneyMack <tosimiti7@gmail.com>
2023-06-25 03:03:47 +0900

    前処理するコード書いた(未検証)

--- aiwolfk2b/AttentionReasoningAgent/Modules/preprocess_data.py
+++ aiwolfk2b/AttentionReasoningAgent/Modules/preprocess_data.py
@@ -3,6 +3,7 @@ from typing import List,Tuple,Dict
 import os,re,time,requests,random
 from collections import defaultdict
 from pathlib import Path
+import pathlib
 
 from aiwolf.gameinfo import GameInfo,_GameInfo
 from aiwolf.gamesetting import GameSetting,_GameSetting
@@ -84,7 +85,7 @@ class GameLogPreprocessor:
     def preprocess_role_info(self)-> Dict[str,Role]:
         div_d1221 = self.soup.find_all("div", class_="d1221")
         if not len(div_d1221) == 1:
-            raise Exception("1つあるべきプレイヤーリストのdivが1つではありません")
+            raise Exception(f"1つあるべきプレイヤーリストのdivが1つではありません:{len(div_d1221)}")
 
         #各プレイヤーの情報を取得する
         div_d1221 = div_d1221[0]
@@ -387,6 +388,8 @@ class GameLogPreprocessor:
             _utterrance["agent"] = agent.agent_idx
             _utterrance["idx"] = speaker_idx
             prep_message = self.preprocess_message(message)
+            if prep_message == "": #空文字の場合は無視する
+                return 
             _utterrance["text"] = prep_message
             _utterrance["turn"] = -1
             self._game_info_day_dict[self.day]["talkList"].append(_utterrance)
@@ -583,27 +586,168 @@ class GameLogPreprocessor:
         
 
 def unit_test_GameLogPreprocessor():
-    import pathlib
     path = pathlib.Path(__file__).resolve().parent
     inputpath = path.joinpath("sample_log_raw.txt")
     outputpath = path.joinpath("sample_log_preprocessed.txt")
-    parser = GameLogPreprocessor()
+    parser = GameLogPreprocessor(criteria_agent_idx=2)
     parser.get_data_from_file(inputpath)
     parser.parse()
     print(parser._game_setting)
     for game_info_day in parser._game_info_day_dict.values():
-        for talk in game_info_day["talkList"]:
+        for talk in game_info_day["whisperList"]:
             print(talk)
         #print(game_info_day["talkList"])
+        
 
 def output_file(output_filepath: str, output_buffer: List[str]):
     with open(output_filepath, 'w+') as file:
         for data in output_buffer:
             file.write(data + "\n")
+            
+def distil_data(inputdir:Path,outputdir:Path):
+    # 指定したディレクトリにあるデータを読み込む
+    import shutil
+    log_grob = "log_*.txt"
+    #ファイルを1つづつ取り出して処理を行う
+    for inputpath in inputdir.rglob(log_grob):
+        try:
+            parser = GameLogPreprocessor(criteria_agent_idx=2)
+            parser.get_data_from_file(inputpath)
+            parser.parse()
+            # 正しく実行ができるものは別ディレクトリにコピー
+            #特に、人数が8人以下のもの
+            if parser._game_setting["agentNum"] <= 8:
+                shutil.copyfile(inputpath, outputdir.joinpath(inputpath.name))
+            
+        except Exception as e:
+            #print(e)
+            pass
 
+
+def make_dataset(inputdir:Path,outputdir:Path):
+    import csv,pickle
+    dataset = []
+    # 指定したディレクトリにあるデータを読み込む
+    log_grob = "log_*.txt"
+    for inputpath in inputdir.rglob(log_grob):
+        parser_base = GameLogPreprocessor(criteria_agent_idx=2)
+        parser_base.get_data_from_file(inputpath)
+        parser_base.parse()
+        playerNum = parser_base._game_setting["playerNum"]
+        #9人以上のゲームは除外
+        if playerNum > 8:
+            continue
+        
+        #各エージェントの立場から、別のエージェントの役職を推定する
+        for from_agent_idx in range(1,playerNum+1):
+            for to_agent_idx in range(1,playerNum+1):
+                #自分自身は除く
+                if from_agent_idx == to_agent_idx:
+                    continue
+                
+                parser = GameLogPreprocessor(my_agent_idx=from_agent_idx,criteria_agent_idx=to_agent_idx)
+                parser.soup = parser_base.soup #BeautifulSoupのインスタンスを共有することで高速化
+                parser.parse()
+        
+                agent_role_dict = parser.agent_role_dict
+                game_setting = GameSetting(parser._game_setting)
+                game_info_day_list = [GameInfo(_game_info_day) for _game_info_day in parser._game_info_day_dict.values()]
+                #モデル入力用の形式に変換
+                input_text = ""
+                
+                #最初にゲームの役職分布を書く
+                role_list = [Role.VILLAGER,Role.SEER,Role.BODYGUARD,Role.MEDIUM,Role.WEREWOLF,Role.POSSESSED,Role.FOX,Role.FREEMASON]
+                role_text = str(game_setting.role_num_map[Role.VILLAGER])
+                for role in Role[1:]:
+                    role_text += ","+str(game_setting.role_num_map[role])
+                role_text+="\n"
+                
+                #自分の役職
+                my_role_text = str(agent_role_dict[from_agent_idx]) + "\n"
+                #会話文を書く
+                daily_text = ""
+                for idx,game_info in enumerate(game_info_day_list[1:]):
+                    talk_list = game_info.talk_list
+                    #Whisper_list = game_info.whisper_list
+                    vote_list = game_info.vote_list
+                    divine_result = game_info.divine_result
+                    attacked_agent = game_info.attacked_agent
+                    executed_agent = game_info.executed_agent
+                    guarded_agent = game_info.guarded_agent
+                    
+                                        
+                    day_text = f"day{idx+1}\n"
+                    talk_text = ""
+                    vote_text = ""
+                    divine_result_text = ""
+                    attacked_agent_text = ""
+                    guarded_agent_text = ""
+                    executed_agent_text = ""
+                    
+                    if len(talk_list) > 0:
+                        for talk in reversed(talk_list):
+                            one_talk_text = f"talk,{talk.idx},{talk.text}\n"
+                            talk_text += one_talk_text
+                    if len(vote_list) > 0:
+                        for vote in vote_list:
+                            one_vote_text = f"vote,{vote.agent.agent_idx},{vote.target.agent_idx}\n"
+                            vote_text += one_vote_text
+                    if divine_result is not None:
+                        divine_result_text = f"divine,{divine_result.agent.agent_idx,divine_result.target.agent_idx,str(divine_result.result)}\n"
+                    if attacked_agent is not None:
+                        attacked_agent_text = f"attacked,{attacked_agent.agent_idx}\n"
+                    if guarded_agent is not None:
+                        guarded_agent_text = f"guarded,{guarded_agent.agent_idx}\n"
+                    if executed_agent is not None:
+                        executed_agent_text = f"executed,{executed_agent.agent_idx}\n"
+                    
+                    daily_text += day_text + divine_result_text + attacked_agent_text + guarded_agent_text+ talk_text + vote_text + executed_agent_text
+                    
+                input_text = role_text + my_role_text + daily_text
+                answer_text = str(agent_role_dict[Agent(to_agent_idx)])
+                dataset.append((answer_text,input_text))
+    #ファイルに書き込む
+    write = csv.writer(open(outputdir.joinpath("dataset.csv") , "w"))
+    write.writerows(dataset)
+    
+    with open(outputdir.joinpath("dataset.pkl"), "wb") as f:
+        pickle.dump(dataset, f)
+        
 if __name__ == '__main__':
-    unit_test_GameLogPreprocessor()
+    #unit_test_GameLogPreprocessor()   
+    
+    current_dir = pathlib.Path(__file__).resolve().parent
+    inputdir = current_dir.joinpath("raw_data")
+    outputdir = current_dir.joinpath("preprocessable_data_max8")
+    distil_data(inputdir,outputdir)
 
+    #蒸留したデータを読み込んで、前処理を行い、１つのファイルにまとめる
+    inputdir = outputdir
+    outputdir = current_dir.joinpath("preprocessed_data_max8")
+    make_dataset(inputdir,outputdir)
+    
+    
+    # import shutil
+    # log_grob = "log_*.txt"
+    # #ファイルを1つづつ取り出して処理を行う
+    # for inputpath in inputdir.rglob(log_grob):
+    #     try:
+    #         parser = GameLogPreprocessor(criteria_agent_idx=2)
+    #         parser.get_data_from_file(inputpath)
+    #         parser.parse()
+    #         # print(parser._game_setting)
+    #         # for game_info_day in parser._game_info_day_dict.values():
+    #         #     for talk in game_info_day["talkList"]:
+    #         #         print(talk)
+    #         # 正しく実行ができるものは別ディレクトリにコピー
+    #         #特に、人数が8人以下のもの
+    #         if parser._game_setting["agentNum"] <= 8:
+    #             shutil.copyfile(inputpath, outputdir.joinpath(inputpath.name))
+            
+    #     except Exception as e:
+    #         #print(e)
+    #         pass
+        
     # url_prefix = "https://ruru-jinro.net/"
 
     # #スクレイピング
