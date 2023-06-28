c819364c3e013a6851df1466f0f24d136b7b6cd7
HoneyMack <tosimiti7@gmail.com>
2023-06-28 10:49:34 +0900

    BERTへの入力を生成する関数の追加(途中)

--- aiwolfk2b/AttentionReasoningAgent/Modules/BERTRoleEstimationModel.py
+++ aiwolfk2b/AttentionReasoningAgent/Modules/BERTRoleEstimationModel.py
@@ -20,7 +20,7 @@ class BERTRoleEstimationModel(AbstractRoleEstimationModel):
         self.batch_size = config.getint("RoleEstimationModel","batch_size")
         self.max_length = config.getint("RoleEstimationModel","max_length")
         #役職推定に使うラベルのリスト(順番に意味あり)
-        self.labels_list = [Role.VILLAGER,Role.SEER,Role.BODYGUARD,Role.MEDIUM,Role.WEREWOLF,Role.POSSESSED,Role.FOX,Role.FREEMASON]
+        self.role_label_list = [Role.VILLAGER,Role.SEER,Role.BODYGUARD,Role.MEDIUM,Role.WEREWOLF,Role.POSSESSED,Role.FOX,Role.FREEMASON]
         
         #計算に使うdeviceを取得
         #self.device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -37,21 +37,111 @@ class BERTRoleEstimationModel(AbstractRoleEstimationModel):
         self.game_info = game_info
         self.game_setting = game_setting
         
-    def estimate(self,agent:Agent, game_info: GameInfo, game_setting: GameSetting) -> RoleEstimationResult:
-        """bertを使って役職を推定する"""
-        #１人以上存在する役職のリスト
-        role_list = [role for role in game_setting.role_num_map.keys() if game_setting.role_num_map[role] > 0]
-        estimation = {}
-        scores = {}
-        for role in role_list:
-            scores[role] = 10*random.random() -5.0
+    def make_estimation_text(self,agent: Agent, game_info_list: List[GameInfo], game_setting: GameSetting,preprocess_text:bool=True)->str:
+        """
+        役職推定に使うテキストを作成する
+        
+        Parameters
+        ----------
+        agent : Agent
+            推定対象のエージェント
+        game_info : List[GameInfo]
+            役職推定に用いるゲーム情報
+        game_setting : GameSetting
+            役職推定に用いるゲーム設定
+        
+        Returns
+        -------
+        str
+            役職推定に使うテキスト
+        """
+        
+        #推論に必要な情報をテキスト化
+        #書式は以下の通り
+        #role_map:(villager,seer,bodyguard,medium,werewolf,possessed,fox,cat,immortal,freemason) 
+        #day1
+        #attacked,agent_idx
+        #guareded,agent_idx
+        #(divine,agent_idx,species) #あれば
+        #talk
+        #unique_id,agent_idx,text
+        #vote
+        #from_agent_idx,to_agent_idx
+        #executed,agent_idx
+        #day2
+        # same as day1
+        #...
+        #my_role (あれば)
+        
+        def rotate_agent_idx(agent_idx:int)->int:
+            """
+            推定するエージェントの番号が01になるようにインデックスを入れ替える
+
+            Parameters
+            ----------
+            agent_idx : int
+                入れ替えるエージェントの番号
+
+            Returns
+            -------
+            int
+                入れ替えた後のエージェントの番号
+            """
+            return (agent_idx - agent.agent_idx + game_setting.player_num)%game_setting.player_num+1
+        
+        estimation_text = ""
+        #役職のマップを作成
+        role_text=str(game_setting.role_num_map[self.role_label_list[0]])
+        for role in self.role_label_list[1:]:
+            role_text += ","+str(game_setting.role_num_map[role])
             
-        #ソフトマックス関数で正規化
-        sum_exp = sum([math.exp(v) for v in scores.values()])
-        for role in role_list:
-            estimation[role] = math.exp(scores[role])/sum_exp
-            
-        return RoleEstimationResult(agent,estimation,None)
+        estimation_text += "role_map:("+role_text+")\n"
+        
+        #日付ごとに情報をまとめる
+        for game_info in game_info_list:
+            estimation_text += f"day{game_info.day}\n"
+            #朝わかる結果を追加
+            if game_info.divine_result is not None: #占い結果があれば
+                target_agent_idx = rotate_agent_idx(game_info.divine_result.target.agent_idx)
+                species = game_info.divine_result.result
+                estimation_text += f"divine,{target_agent_idx},{species}\n"
+            if game_info.attacked_agent is not None: #襲撃結果があれば
+                agent_idx = rotate_agent_idx(game_info.attacked_agent.agent_idx)
+                estimation_text += f"attacked,{agent_idx}\n"
+                
+                
+            #会話文をテキスト化
+            day_text=""
+        
+        
+        
+        #推定対象のエージェントをAgent[01]とするように順番を入れ替えた上で、Agent[数字]->[数字]に変換
+          
+    
+    def estimate(self,agent:Agent, game_info_list: List[GameInfo], game_setting: GameSetting,preprocess_text:bool=True) -> RoleEstimationResult:
+        """
+        BERTを使って役職を推定する
+
+        Parameters
+        ----------
+        agent : Agent
+            推定対象のエージェント
+        game_info : List[GameInfo]
+            役職推定に用いるゲーム情報
+        game_setting : GameSetting
+            役職推定に用いるゲーム設定
+        preprocess_text : bool, optional
+            GameInfoのTalkを前処理するかどうか(True:する), by default True
+
+        Returns
+        -------
+        RoleEstimationResult
+            Agentの役職推定結果
+        """
+        
+        
+        
+        pass
     
     def estimate_from_text(self,text_list:List[str],game_setting: GameSetting=None)-> List[RoleEstimationResult]:
         """
@@ -87,7 +177,7 @@ class BERTRoleEstimationModel(AbstractRoleEstimationModel):
             scores = outputs.logits
             attentions = outputs.attentions
             #存在しない役職のスコアを-torch.infにする
-            for idx,role in enumerate(self.labels_list):
+            for idx,role in enumerate(self.role_label_list):
                 if game_setting.role_num_map[role] == 0:
                     scores[:,idx] = -torch.inf
             
@@ -96,8 +186,8 @@ class BERTRoleEstimationModel(AbstractRoleEstimationModel):
             probs = probs.cpu().double().numpy()
             for i in range(len(text_list)):
                 estimation = {}
-                for j in range(len(self.labels_list)):
-                    estimation[self.labels_list[j]] = probs[i][j]
+                for j in range(len(self.role_label_list)):
+                    estimation[self.role_label_list[j]] = probs[i][j]
                 one_batch_attention = [attentions[k][i].cpu().numpy() for k in range(len(attentions))]
                 results.append(RoleEstimationResult(None,estimation,one_batch_attention))
             
@@ -132,7 +222,17 @@ if __name__ == "__main__":
      
     estimator = BERTRoleEstimationModel(config_ini)
     estimator.initialize(game_info,game_setting)
-    result_list = estimator.estimate_from_text(["今日は晴れですね","明日は雨です"])
+    
+    test_text =["""2,1,0,0,1,1,0,0
+SEER
+day1
+talk:
+占いCO[01]は人狼
+そうだ、俺は人狼だ！
+"""]                
+
+    
+    result_list = estimator.estimate_from_text(test_text)
     for res in result_list:
         print(res.probs)
         
