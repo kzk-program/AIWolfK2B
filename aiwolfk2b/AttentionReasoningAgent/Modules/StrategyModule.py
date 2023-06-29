3806437ef3967b551dc7f040abebe68aca1defbf
kzk-program <carzookey923@docomo.ne.jp>
2023-06-29 20:43:27 +0900

    プルリクレビュー　型ヒントエラー解決・alive・max_prob初期値・raise・mainテスト

--- aiwolfk2b/AttentionReasoningAgent/Modules/StrategyModule.py
+++ aiwolfk2b/AttentionReasoningAgent/Modules/StrategyModule.py
@@ -3,7 +3,10 @@ from typing import List,Tuple,Dict,Any,Union
 
 from aiwolf import GameInfo, GameSetting
 from aiwolf.agent import Agent,Role
-from aiwolfk2b.AttentionReasoningAgent.AbstractModules import AbstractRoleEstimationModel,AbstractStrategyModule,AbstractRoleInferenceModule,RoleInferenceResult,OneStepPlan
+
+from aiwolfk2b.AttentionReasoningAgent.AbstractModules import AbstractRoleEstimationModel,AbstractStrategyModule,AbstractRoleInferenceModule,RoleInferenceResult,OneStepPlan, ActionType
+import errno
+import os
 
 class StrategyModule(AbstractStrategyModule):
     """戦略立案モジュール"""
@@ -21,7 +24,7 @@ class StrategyModule(AbstractStrategyModule):
         """投票"""
         #各エージェントの役職を推定する
         inf_results:List[RoleInferenceResult] = []
-        for a in game_info.agent_list:
+        for a in game_info.alive_agent_list:
             inf_results.append(self.role_inference_module.infer(a, game_info, game_setting))
 
         #エージェントの中から最も占い師の確率が高いエージェントを選ぶ
@@ -49,7 +52,7 @@ class StrategyModule(AbstractStrategyModule):
         """襲撃"""
         #各エージェントの役職を推定する
         inf_results:List[RoleInferenceResult] = []
-        for a in game_info.agent_list:
+        for a in game_info.alive_agent_list:
             inf_results.append(self.role_inference_module.infer(a, game_info, game_setting))
 
         #エージェントの中から最も占い師の確率が高いエージェントを選ぶ
@@ -69,7 +72,7 @@ class StrategyModule(AbstractStrategyModule):
         """占い"""
         #各エージェントの役職を推定する
         inf_results:List[RoleInferenceResult] = []
-        for a in game_info.agent_list:
+        for a in game_info.alive_agent_list:
             inf_results.append(self.role_inference_module.infer(a, game_info, game_setting))
 
         #人狼側である確率が最も低いエージェントを選ぶ
@@ -81,12 +84,12 @@ class StrategyModule(AbstractStrategyModule):
     def guard(self, game_info: GameInfo, game_setting: GameSetting) -> Agent:
         """護衛"""
         """５人人狼では不要"""
-        return
+        raise NotImplementedError
     
     def whisper(self, game_info: GameInfo, game_setting: GameSetting) -> str:
         """人狼同士の相談"""
         """５人人狼では不要"""
-        return
+        raise NotImplementedError
         
     def plan(self, game_info: GameInfo, game_setting: GameSetting) -> None:
         """行動計画"""
@@ -105,7 +108,7 @@ class StrategyModule(AbstractStrategyModule):
         """推定結果から最も役職の確率の高いエージェントを選ぶ"""
         w = self.weight(role)
 
-        max_prob = 0
+        max_prob = -1
         max_agent = None
         for inf in inf_list:
             p = [inf.probs[Role.VILLAGER],inf.probs[Role.SEER],inf.probs[Role.POSSESSED],inf.probs[Role.WEREWOLF]]
@@ -121,7 +124,7 @@ class StrategyModule(AbstractStrategyModule):
         """推定結果から最も役職の確率の高いエージェントを選ぶ"""
         w = self.weight(role)
 
-        min_prob = 1
+        min_prob = 2
         min_agent = None
         for inf in inf_list:
             p = [inf.probs[Role.VILLAGER],inf.probs[Role.SEER],inf.probs[Role.POSSESSED],inf.probs[Role.WEREWOLF]]
@@ -133,7 +136,7 @@ class StrategyModule(AbstractStrategyModule):
                 min_agent = inf.agent
         return min_agent
     
-    def weight(self, role: Role) -> List[float, float, float, float]:
+    def weight(self, role: Role) -> List[float]:
         """重みつき：[村人,占い師,狂人,人狼]"""
         if role == Role.VILLAGER:
             return [1,0,0,0]
@@ -157,3 +160,30 @@ class StrategyModule(AbstractStrategyModule):
         else:
             return False
         
+if __name__=="__main__":
+    import pickle
+    from aiwolf.agent import Status
+    from aiwolfk2b.AttentionReasoningAgent.SimpleModules import RandomRoleEstimationModel, SimpleRoleInferenceModule
+    config_ini = ConfigParser()
+    config_ini_path = os.pardir + '/config.ini'
+
+    # iniファイルが存在するかチェック
+    if os.path.exists(config_ini_path):
+        # iniファイルが存在する場合、ファイルを読み込む
+        with open(config_ini_path, encoding='utf-8') as fp:
+            config_ini.read_file(fp)
+    else:
+        # iniファイルが存在しない場合、エラー発生
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_ini_path)
+    
+    role_estimation_model = RandomRoleEstimationModel(config_ini)
+    role_inference_module = SimpleRoleInferenceModule(config_ini, role_estimation_model)
+    with open(os.pardir +"/game_info.pkl", mode="rb") as f:
+        game_info:GameInfo = pickle.load(f)
+    with open(os.pardir + "/game_setting.pkl", mode="rb") as f:
+        game_setting:GameSetting = pickle.load(f)
+    
+    strategy_module = StrategyModule(config_ini, role_estimation_model, role_inference_module)
+    strategy_module.initialize(game_info, game_setting)
+    game_info.status_map= {Agent(1):Status.ALIVE, Agent(2):Status.ALIVE, Agent(3):Status.ALIVE, Agent(4):Status.ALIVE, Agent(5):Status.ALIVE}
+    print(strategy_module.vote(game_info, game_setting))
