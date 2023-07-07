caf07e8612455e34c9bf86fbf47cc4de71e269c2
mamsong <katori.shiong@gmail.com>
2023-07-07 23:11:36 +0900

    SpeakerModule変更+configのcharacter設定変更

--- aiwolfk2b/AttentionReasoningAgent/Modules/SpeakerModule.py
+++ aiwolfk2b/AttentionReasoningAgent/Modules/SpeakerModule.py
@@ -22,8 +22,14 @@ class SpeakerModule(AbstractSpeakerModule):
             return "Skip"
         if "Over" in speech:
             return "Over"
-        messages = [{"role":"system", "content": f"入力される文章を{self.character}のキャラ付けに変換してください。Agent[数字]という表現は変えないでください。"},
+        messages = [{"role":"system", "content": f"""
+                        この文章を{self.character}に変換してください。
+                        基本的に変えていいのは口調のみです。同じ言葉を繰り返すことは避けてください。
+                        また、入力される文章を変え過ぎないでください。
+                        Agent[数字]という表現は変えないでください。Agentと[数字]の間に言葉を入れないでください。(例:Agent[01],Agent[02],Agent[03],Agent[04],Agent[05])
+                        """},
                     {"role": "user", "content":speech}]
+        
         response = self.chatgpt_api.complete(messages)
         return response
     
@@ -35,6 +41,6 @@ if __name__=="__main__":
     
     speaker_module = SpeakerModule(config)
     speaker_module.initialize(game_info,game_setting)
-    
-    
+
+
     print(speaker_module.enhance_speech("Agent[01]が人狼だと思います。皆さん、Agent[01]に投票しましょう。"))
