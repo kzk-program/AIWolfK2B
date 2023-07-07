c7b7bd485f88f284fc894670f6f14d1cde6cc58d
kzk-program <carzookey923@docomo.ne.jp>
2023-07-08 01:14:10 +0900

    「星詠み」などの訳わからない言い換えを防止するための策を追加

--- aiwolfk2b/AttentionReasoningAgent/Modules/SpeakerModule.py
+++ aiwolfk2b/AttentionReasoningAgent/Modules/SpeakerModule.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 from typing import List,Tuple,Dict,Any,Union
 
-from aiwolf import GameInfo,GameSetting
+from aiwolf import GameInfo,GameSetting, Agent
 
 from aiwolfk2b.AttentionReasoningAgent.AbstractModules import AbstractSpeakerModule
 from aiwolfk2b.AttentionReasoningAgent.Modules.GPTProxy import ChatGPTAPI
@@ -25,8 +25,9 @@ class SpeakerModule(AbstractSpeakerModule):
         messages = [{"role":"system", "content": f"""
                         この文章を{self.character}に変換してください。
                         基本的に変えていいのは口調のみです。同じ言葉を繰り返すことは避けてください。
-                        また、入力される文章を変え過ぎないでください。
+                        また、入力される文章を変え過ぎないでください。特に、「占い師」「人狼」「村人」などの役職の表現はそのままにしてください。
                         Agent[数字]という表現は変えないでください。Agentと[数字]の間に言葉を入れないでください。(例:Agent[01],Agent[02],Agent[03],Agent[04],Agent[05])
+                        >>Agent[数字]という表現は変えないでください。>>Agentと[数字]の間に言葉を入れないでください。(例:>>Agent[01],>>Agent[02],>>Agent[03],>>Agent[04],>>Agent[05])
                         """},
                     {"role": "user", "content":speech}]
         
@@ -38,9 +39,12 @@ if __name__=="__main__":
     config =  load_default_config()
     game_info = load_default_GameInfo()
     game_setting = load_default_GameSetting()
-    
+    game_info.me = Agent(1)
     speaker_module = SpeakerModule(config)
     speaker_module.initialize(game_info,game_setting)
 
 
-    print(speaker_module.enhance_speech("Agent[01]が人狼だと思います。皆さん、Agent[01]に投票しましょう。"))
+    print(speaker_module.enhance_speech("Agent[02]が人狼だと思います。皆さん、Agent[02A]に投票しましょう。"))
+    print(speaker_module.enhance_speech("占い師COします。Agent[02]を占った結果、人狼でした。"))
+    print(speaker_module.enhance_speech("占い師の人がいたらCOしてほしいなあ。"))
+    print(speaker_module.enhance_speech(">>Agent[03] Agent[02]を占ったのは、1日目の発言が怪しかったから人狼だと疑わしかったからです。"))
